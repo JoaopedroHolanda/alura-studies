2ee87e09bc772365136ac0a15f1a985ba2e6ba86
João Pedro <126481770+JoaopedroHolanda@users.noreply.github.com>
2024-10-21 21:38:53 -0300

    Subindo código para o github

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,17 @@
+import style from "./Botao.module.scss";
+
+interface Props {
+  texto: string;
+  type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void;
+}
+
+function Botao({ onClick, texto, type }: Props) {
+  return (
+    <button onClick={onClick} type={type} className={style.botao}>
+      {texto}
+    </button>
+  );
+}
+
+export default Botao;
